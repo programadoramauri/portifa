2c929834b3871438618d5dba4eb609615307977b
Amauri Franco <programador.amauri@gmail.com>
2015-02-11 09:06:37 -0200

    Colocando um pouco de código nos arquivos. Estrutura básica pronta.

---
+++ header.php
@@ -0,0 +1,10 @@
+<html>
+	<head>
+		<title>Programador Amauri</title>
+		<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url') ?>">
+	</head>
+	<body>
+		<div id="wrapper">
+			<div id="header">
+				<h1>HEADER</h1>
+			</div>
